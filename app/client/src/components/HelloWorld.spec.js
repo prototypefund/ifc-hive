374cf0c122950e20c3d1cd8b17a3394eb1e1f525
Daniel Kaminski <daniel@karo.berlin>
2022-05-10 06:56:05 +0200

    feat (client) #39: Add eslint plugin for cypress
    - add eslint cypress plugin so we don't have to register all global
      variables.

--- app/client/src/components/HelloWorld.spec.js
+++ app/client/src/components/HelloWorld.spec.js
@@ -1,12 +1,14 @@
 import { mount } from '@cypress/vue'
 import HelloWorld from './HelloWorld.vue'
 
-it('renders a message from prop data', () => {
-  mount(HelloWorld, {
-    propsData: {
-      msg: 'Hello Cypress!',
-    },
-  })
+describe('HelloWorl', () => {
+  it('renders a message from prop data', () => {
+    mount(HelloWorld, {
+      propsData: {
+        msg: 'Hello Cypress!',
+      },
+    })
 
-  cy.get('h1').contains('Hello Cypress!')
+    cy.get('h1').contains('Hello Cypress!')
+  })
 })
