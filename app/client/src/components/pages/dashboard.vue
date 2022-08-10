5eb5a461e8414d4bf67f3f56adb5a0cfc2db4b93
Daniel Kaminski <daniel@karo.berlin>
2022-08-10 17:43:01 +0200

    feat #51: added demo for column count change

--- app/client/src/components/pages/dashboard.vue
+++ app/client/src/components/pages/dashboard.vue
@@ -4,7 +4,14 @@
     <p>url params > {{ props.urlParams }} &lt; click value {{ state.count }}</p>
     <div class="mb-10">
       <v-btn @click="counter">addCount</v-btn>
-      <v-btn @click="changeGrid">changeGrid</v-btn>
+      <v-btn @click="changeGridType">changeGrid Type</v-btn>
+      <v-btn @click="changeGridItem">changeGrid item</v-btn>
+    </div>
+    <div class="mb-10">
+      <v-btn @click="changeGridColumnCount(1)">changeGrid ColumnCount to 1</v-btn>
+      <v-btn @click="changeGridColumnCount(2)">changeGrid ColumnCount to 2</v-btn>
+      <v-btn @click="changeGridColumnCount(3)">changeGrid ColumnCount to 3</v-btn>
+      <v-btn @click="changeGridColumnCount(4)">changeGrid ColumnCount to 4</v-btn>
     </div>
     <Grid v-if="state.slots" />
   </v-container>
@@ -54,15 +61,38 @@ function counter() {
     },
   });
 }
-const changeGrid = () => {
+const changeGridType = () => {
   let grid = {
     type: "default",
-    items: "card",
-    columns: 3,
   };
   if (state.value.grid.type === "default") {
     grid.type = "dark";
   }
+  $store.dispatch({
+    type: "currentPage/update",
+    payload: {
+      grid,
+    },
+  });
+};
+const changeGridItem = () => {
+  let grid = {
+    items: "card",
+  };
+  if (state.value.grid.items === "card") {
+    grid.items = "card_flat";
+  }
+  $store.dispatch({
+    type: "currentPage/update",
+    payload: {
+      grid,
+    },
+  });
+};
+const changeGridColumnCount = (count) => {
+  let grid = {
+    columns: count,
+  };
   $store.dispatch({
     type: "currentPage/update",
     payload: {
