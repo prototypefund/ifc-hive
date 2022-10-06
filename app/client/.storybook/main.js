8e14420c3fe00f9fd147d72404b4bb9cd7a99d5f
Daniel Kaminski <daniel@karo.berlin>
2022-10-06 16:07:25 +0200

    feat #58: added storybook to current code base, changed dependencies and fixed config

---
+++ app/client/.storybook/main.js
@@ -0,0 +1,30 @@
+// use `mergeConfig` to recursively merge Vite options
+const { mergeConfig } = require('vite');
+
+module.exports = {
+  async viteFinal(config, { configType }) {
+    // return the customized config
+    return mergeConfig(config, {
+      // customize the Vite config here
+      resolve: {
+        alias: { foo: 'bar' },
+      },
+    });
+  },
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/vue3",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true
+  }
+}
