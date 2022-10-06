5fafc638f3ba6637f76a86e7ef80701f545825ad
Joshua Schäfer <jota89@gmx.de>
2022-10-06 16:37:16 +0200

    add samples

---
+++ app/client/src/stories/DemoEnv.stories.js
@@ -0,0 +1,36 @@
+
+import getEnvVariable from '../lib/getEnvVariable'
+
+
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Demo/getEnvVariable',
+ // component: MyButton,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    onClick: {},
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+ 
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`  v-bind="args" 
+  template: '<p>Template args</p> <ol> <li v-for="(value, key, index) in args"> {{key}} : \"{{value}}\" </li><ol>',
+});
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button',
+  API_BASE_URL:    getEnvVariable('VITE_API_BASE_URL'),
+  NODE_ENV: 'NODE_ENV' +  getEnvVariable('NODE_ENV'),
+};
