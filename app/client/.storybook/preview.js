37958fec2eaecb19147a1006b42e30e956743e71
Daniel Kaminski <daniel@karo.berlin>
2022-10-11 21:15:25 +0200

    feat #58: added dark Theme, because its important

--- app/client/.storybook/preview.js
+++ app/client/.storybook/preview.js
@@ -4,7 +4,17 @@ import i18n from '../src/setup/i18n.js'
 import vuetify from '../src/setup/vuetify.js'
 import axios from 'axios'
 import filters from '../src/setup/filters.js'
+import { themes } from '@storybook/theming';
 
+export const parameters = {
+    darkMode: {
+        // Override the default dark theme
+        dark: { ...themes.dark, appBg: 'black' },
+        // Override the default light theme
+        light: { ...themes.normal },
+        current: 'dark'
+    }
+};
 
 app.use(i18n)
 app.use(vuetify)
