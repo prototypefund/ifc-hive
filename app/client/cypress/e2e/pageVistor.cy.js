7ca1e9cdf07a9e5df7492c86545f0b89cb73b969
Joshua Schäfer <jota89@gmx.de>
2022-10-19 16:26:28 +0200

    feat #58: clean Tests and fix sbHelper - sbHelper can use more Varnames - add more Tests.

--- app/client/cypress/e2e/pageVistor.cy.js
+++ app/client/cypress/e2e/pageVistor.cy.js
@@ -1,62 +1,12 @@
 
 
 const runTest = (name, isComonentTest = true) => {
-  console.log('prepare')
-  var DATA = "DATATATATAAT"
-  Cypress.Commands.add('asComponentTest', (data) => { throw "BAD DEFINITION" })
-  Cypress.Commands.add('context', () => { return "A" })
-  window.testExecutionContex = () => { return { isComonentTest: isComonentTest } }
   if (isComonentTest) {
     var rq = require(`../../src/components/pages/${name}.component.cy.js?isComonentTest0=A`)
   } else {
     var rq = require(`../../src/components/pages/${name}.component.cy.js?isComonentTest0=B`)
   }
-
 }
 
 
-/*
-describe("Visit Page", () => {
-  it("visits the app root url dual TEST", () => {
-    prepareComponent('dashboard')
-    cy.asComponentTest("")
-  })
-})
-*/
-/*
 
-describe("Visit Page", () => {
-  beforeEach(() => {
-    cy.fixture('visit.json').as('visitPage')
-  }),
-    it("visits the app root url dual TEST", () => {
-
-      cy.get('@visitPage').then(
-        x => {
-          cy.log(x)
-        }
-      );
-
-      prepareComponent('dashboard')
-      // cy.asComponentTest('465464')
-      //prepareComponent('testboard')
-      //cy.asComponentTest('999999')
-
-      cy.log("Done")
-    })
-
-})*/
-
-console.log('aaaaaaaaaaaaaaaaaaaaaaaaaa')
-runTest('dashboard', false)
-console.log('bbbbbbbbbbbbbbbbbbbbbbbbbb')
-/*
-describe("Visit Page", () => {
-  it.only("visits the app root url dual TEST", () => {
-    cy.log('---')
-  })
-})*/
-
-runTest('dashboard', true)
-console.log('cccccccccccccccccccccccccc')
-// cy.asComponentTest("")
--- app/client/src/components/utils/navigation/sidebar.component.cy.js
+++ app/client/src/components/utils/navigation/sidebar.component.cy.js
@@ -3,53 +3,26 @@ import { isComonentTest } from '../../pages/helper.js'
 
 
 describe("Navigate True Sidebar", () => {
-  it.only("visits Sidebar", () => {
+  it("visits Sidebar", () => {
     if (isComonentTest()) {
-      cy.visitSB('Pages/Dashboard', 'Headless')
-    } else {
-      cy.visit("/");
-    }
-
-    /**
-     * 
-     * Wie impoprtiern der zu erwartenden Strings ? 
-     */
-    cy.get('[data-test-id]')
-    cy.get('[data-test-id="sidebar_nav-app-settings"]').click()
-    cy.get('[data-test-id="sidebar_nav-app-testboard"]').click()
-
-    // cy.get('[data-test-id="sidebar_nav-app-daniel"]').click()
-    cy.get('[data-test-id="sidebar_nav-app-journal"]').click()
-    cy.get('[data-test-id="sidebar_nav-app-projects"]').click()
-    cy.get('[data-test-id="sidebar_nav-app-dashboard"]').click()
-
-
-
-    cy.contains("h1", "Dashboard - funoFun");
-  });
-
-  it("visits the app root and go to about page", () => {
-    if (isComonentTest()) {
-      cy.visitSB('Pages/Testboard', 'Full')
-      // Wir müssen immer eine abfrage machen 
+      cy.visitSB('Pages/Dashboard', 'Full')
       cy.get('[data-test-id]');
     } else {
       cy.visit("/");
-      cy.get('.v-list > :nth-child(4)').click();
+      cy.get('[data-test-id]');
+      /**
+       *  
+       * Wie impoprtiern der zu erwartenden Strings ? 
+       */
+      cy.get('[data-test-id="sidebar_nav-app-settings"]').click()
+      cy.get('[data-test-id="sidebar_nav-app-testboard"]').click()
+      cy.get('[data-test-id="sidebar_nav-app-journal"]').click()
+      cy.get('[data-test-id="sidebar_nav-app-projects"]').click()
+      cy.get('[data-test-id="sidebar_nav-app-dashboard"]').click()
     }
 
-    // cy.get('')
-
-    cy.get('.mb-10 > .v-btn').click();
-    cy.get('.mb-10 > .v-btn').click();
-
-    cy.get('.mb-10 > .v-btn').click();
-    cy.get('.mb-10 > .v-btn').click();
-    cy.get('.mb-10 > .v-btn').click();
-    cy.get('.mb-10 > .v-btn').click();
-    cy.get('.mb-10 > .v-btn').click();
-    cy.contains("p", "click value 7");
   });
+
 
 
   it("visits the app root url dual TEST", () => {
