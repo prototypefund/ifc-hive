8c2647186439be867e1363d81dab22af2578116c
Joshua Schäfer <jota89@gmx.de>
2022-10-28 12:44:27 +0200

    feat (client) #58: Extend Doku and help commands

--- app/client/cypress/support/commands.js
+++ app/client/cypress/support/commands.js
@@ -48,3 +48,22 @@ Cypress.Commands.add('visitStorybook', (title, name) => {
         })
     }
 })
+
+Cypress.Commands.add('getInput', (testid) => {
+    return cy.get(`[data-test-id="${testid}"]`, { log: false }).find('input', { log: false })
+})
+
+Cypress.Commands.add('getContainer', (widgetcomponentest) => {
+    // Teste das die Anzal der Wigets stimmt
+    // Teste das die Widgets einzeln Exsitiern
+    cy.get('[data-test-container]')
+        .each(
+            x => {
+                cy.wrap(x)
+                    .invoke('attr', 'data-test-container')
+                    .then(attribs => {
+                        widgetcomponentest.value.push(`${attribs}`)
+                    })
+            }
+        )
+})
--- app/client/cypress/support/sbHelper.js
+++ app/client/cypress/support/sbHelper.js
@@ -1,5 +1,39 @@
 
 /**
+ * Nice To Know and how to avoid name Collisions.
+ * Every Storybook can be accsessd over the normal Webiterface.
+ * The Storry book server renders each strorrie insde an ifram 
+ * under http://.../iframe.html?id={storryID}.
+ * The storryID is dirved, by substitution of chacatters,
+ * from the title and the Variable name exporded by the storry.
+ * 
+ * Where can i find the Existing stories ?
+ * Use the via the server http://.../stories.json
+ * Inside the storybook-static/stories.json
+ * The fastest way is the listStoriesFromClass function.
+ * 
+ * Name and id Collisions Example:
+ * export const HeadlessEditModeBadMail  = ...
+ * export const Headless_EditModeBadMail = ...
+ * Both exports now have the SAME name and same ID Suffix.
+ * id:   <tileprefix>--headless-edit-mode-bad-mail
+ * name:	"Headless Edit Mode Bad Mail"
+ * 
+ * How does storybook deal with Name Collisions ?
+ * This verion @storybook/vue3": "^6.5.12" 
+ * The second exported storry will now shadow the first,
+ * because both are mapped to the same name.
+ * It EVEN disaperes in the Webiterface,
+ * therefore they are NOT reachable by any test
+ * 
+ * How to detect Name Collisions ?
+ * the listStoriesFromClass will return multiple Objects with the same id but diffrent varname's
+ *  { title: "title", varname: "AbC", name: "Ab C", id: "title--ab-c" },
+ *  { title: "title", varname: "Ab_C", name: "Ab C", id: "title--ab-c" }
+ * 
+ */
+
+/**
  * Returns If the we are in an Component Test (Storrybook) Envirnent
  * @returns {true} if we are inside Storrybook
  */
@@ -49,8 +83,8 @@ const getTitleFrom = (nameOrVarname) => {
 const getNameFrom = (nameOrVarname) => {
   var result = nameOrVarname
   result = result.replace(/([A-Z]+)/g, " $1");
+
   result = result[0].toUpperCase() + result.substring(1)
-  // result = result.replace(/([A-Z][a-z])/g, " $1");
   result = result.replace(/([A-Z][a-z])/g, " $1");
   result = result.replace(/(_)/g, " ");
   result = result.replace(/(\/)/g, " ");
