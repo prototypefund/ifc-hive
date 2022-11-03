2c12e992f1f79a66a2b9f8e910295959b1535714
Daniel Kaminski <daniel@karo.berlin>
2022-11-03 15:17:23 +0100

    fix #41: removed v-badge in notification widget due to strange exception

--- app/client/src/components/utils/notifications/default.vue
+++ app/client/src/components/utils/notifications/default.vue
@@ -1,11 +1,23 @@
 <template>
-  <v-menu v-model="toggled" :close-on-click="false" :close-on-content-click="false" location="end" v-if="state"
-    transition="scale-transition" class="notificationDrawer">
+  <v-menu
+    v-model="toggled"
+    :close-on-click="false"
+    :close-on-content-click="false"
+    location="end"
+    v-if="state"
+    transition="scale-transition"
+    class="notificationDrawer"
+  >
     <template v-slot:activator="{ props }">
-      <v-badge overlap :color="state.unreadCount > 0 ? 'error' : 'transparent'" :content="state.unreadCount"
-        offset-y="10px" offset-x="10px">
+      {{ state.unreadCount }}
+      <!--v-badge
+        :color="state.unreadCount > 0 ? 'error' : 'transparent'"
+        :content="state.unreadCount"
+        offset-y="10px"
+        offset-x="10px"
+      >
         <v-btn v-bind="props" icon="mdi-bell" />
-      </v-badge>
+      </-v-badge-->
     </template>
 
     <v-card left width="800">
@@ -14,16 +26,32 @@
           <v-expansion-panel-title>Neu</v-expansion-panel-title>
           <v-expansion-panel-text>
             <v-list density="compact">
-              <v-list-item v-for="(entry, index) in state.items" :key="index" :class="{ hovered: hover == index + 1 }"
-                @mouseover="handleHover(index + 1)" @mouseleave="handleHover(false)">
-                <v-list-item-title class="fullListItem" :class="entry.state">{{ entry }}
+              <v-list-item
+                v-for="(entry, index) in state.items"
+                :key="index"
+                :class="{ hovered: hover == index + 1 }"
+                @mouseover="handleHover(index + 1)"
+                @mouseleave="handleHover(false)"
+              >
+                <v-list-item-title class="fullListItem" :class="entry.state"
+                  >{{ entry }}
                 </v-list-item-title>
                 <template v-slot:append>
                   <v-list-item-avatar end>
-                    <v-btn v-if="entry.state === 'read'" variant="text" color="grey lighten-1"
-                      @click="markUnRead(index)" icon="mdi-circle-small"></v-btn>
-                    <v-btn v-if="entry.state === 'unread'" variant="text" color="grey lighten-1"
-                      @click="markRead(index)" icon="mdi-circle-medium"></v-btn>
+                    <v-btn
+                      v-if="entry.state === 'read'"
+                      variant="text"
+                      color="grey lighten-1"
+                      @click="markUnRead(index)"
+                      icon="mdi-circle-small"
+                    ></v-btn>
+                    <v-btn
+                      v-if="entry.state === 'unread'"
+                      variant="text"
+                      color="grey lighten-1"
+                      @click="markRead(index)"
+                      icon="mdi-circle-medium"
+                    ></v-btn>
                   </v-list-item-avatar>
                 </template>
               </v-list-item>
@@ -95,13 +123,13 @@ const markRead = (index) => {
     },
   });
 };
-onMounted(() => { });
+onMounted(() => {});
 onUnmounted(() => {
   stateSubscriber$.unsubscribe();
 });
 </script>
 <style lang="css">
-.notificationDrawer>.v-overlay__content {
+.notificationDrawer > .v-overlay__content {
   right: 0px !important;
   left: auto !important;
 }
