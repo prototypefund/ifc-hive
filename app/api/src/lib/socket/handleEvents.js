6f2ee27873546e3363711d710bc3d7fd31d1906e
Daniel Kaminski <daniel@karo.berlin>
2023-03-16 16:22:19 +0100

    feat (api) #92: socket exeriment intermediate commit

--- app/api/src/lib/socket/handleEvents.js
+++ app/api/src/lib/socket/handleEvents.js
@@ -5,6 +5,7 @@ import Project from '#src/app/core/model/project/project.model.js'
 import Ticket from '#src/app/journal/model/ticket/ticket.model.js'
 import { createDataPayload } from '#src/lib/dataObjectHelpers.js'
 
+
 export async function registerSocketEvents (app) {
   const { wss } = app
 
@@ -16,22 +17,40 @@ export async function registerSocketEvents (app) {
     wss.emit('hello', { id: socket.id })
 
 
+    /* always send a list with the projects accessible to this user */
     const projects = await Project.find()
     wss.emit('projects/list', { projects } )
 
-    const tickets = await Ticket.find().limit(10)
-    const data = tickets.map(t => {
-      const d = {}
-      d._source = t
-      d._id = t._id
-      d._title = t.title
-      d._path = null,
-      d._project = t.project
-      d._type = 'memo'
-      d._disId = t.disId
-      return d
+      Ticket
+        .find({}, { _id: 1, title: 1, owner: 1, project: 1, tags: 1, disId: 1 })
+        // .limit(100)
+        // .cursor()
+        .on('data', (doc) => {
+          app.log.info({ msg: 'Send socket Package', doc })
+          const { _id, title, disId, project } = doc
+          doc.tags.push('tag-todo')
+          const payload = {
+            _id,
+            _title: title,
+            _disId: disId,
+            _project: project,
+            _source: doc,
+            _type: 'memo'
+          }
+          socket.emit('data', [payload] )
+        })
+
+    /* send dummy data when project data are request by the client */
+    socket.on('requestProjectData', async (projectId) => {
+      Ticket
+        .find({}, { _id: 1, title: 1 })
+        .limit(100)
+        .cursor()
+        .on('data', (doc) => {
+          app.log.info({ msg: 'Send socket Package', doc })
+        })
     })
-    wss.emit('dataTest', data)
+
 
     /* on discconnect */
     socket.on('disconnect', () => {
@@ -61,6 +80,18 @@ export async function registerSocketEvents (app) {
           room: room.id,
           msg: `member left ${socket.id}`
         })
+    })
+
+    socket.on('getProjectData', async (request) => {
+      socket.emit('projectDataConfirmation', {
+        msg: '... Kommt gleich',
+        count: {
+          total: 340,
+          tickets: 300,
+          user: 4,
+          tags: 36,
+        }
+      })
     })
 
     /* on details  */
